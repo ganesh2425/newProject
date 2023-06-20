6fac5f2dfe44de1eb568819f96db355f3e627436
Ganesh <kganesh0445@gmail.com>
2023-06-20 17:21:25 +0530

    done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <>
-    <h3>Hello Bangalore!!!!</h3>
+    <h3>Register form!!</h3>
+    <form>
+      {/* <label>Name:</label> */}
+      <input type='text' placeholder='name'/><br/>
+      <input type='text' placeholder='email'/>
+    </form>
     </>
   );
 }
